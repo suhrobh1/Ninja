45bc88836f2aac60639ccdaf9b41c2c9472ac2f3
Suhrob Hasanov <suhrobhasanov1@gmail.com>
2022-02-01 22:01:15 -0800

    Chaining Methods added

--- User.js
+++ User.js
@@ -7,12 +7,15 @@ class User{
     }
     makeDeposit(amount){
         this.accountBalance += amount;
+        return this;
     }
     makeWithdrawal(amount){
         this.accountBalance -= amount;
+        return this;
     }
     displayBalance(){
         console.log(this.name, this.accountBalance);
+        
     }
     transferAmount(otherUser, amount){
         this.accountBalance -= amount;
@@ -27,23 +30,30 @@ const adder = new User("Adder", "blackAdder@lordship.uk");
 
 percy.name = "Lord Percy";
 
-percy.makeDeposit(200);
-percy.makeDeposit(300);
-percy.makeDeposit(500);
-percy.makeWithdrawal(50);
-percy.displayBalance();
+// percy.makeDeposit(200);
+// percy.makeDeposit(300);
+// percy.makeDeposit(500);
+// percy.makeWithdrawal(50);
+// percy.displayBalance();
 
-baldrick.makeDeposit(100);
-baldrick.makeDeposit(150);
-baldrick.makeWithdrawal(20);
-baldrick.makeWithdrawal(30);
-baldrick.displayBalance();
+percy.makeDeposit(200).makeDeposit(300).makeDeposit(500).makeWithdrawal(50).displayBalance();
 
-adder.makeDeposit(700);
-adder.makeWithdrawal(20);
-adder.makeWithdrawal(40);
-adder.makeWithdrawal(60);
-adder.displayBalance();
+// baldrick.makeDeposit(100);
+// baldrick.makeDeposit(150);
+// baldrick.makeWithdrawal(20);
+// baldrick.makeWithdrawal(30);
+// baldrick.displayBalance();
+
+baldrick.makeDeposit(100).makeDeposit(150).makeWithdrawal(20).makeWithdrawal(30).displayBalance();
+
+// adder.makeDeposit(700);
+// adder.makeWithdrawal(20);
+// adder.makeWithdrawal(40);
+// adder.makeWithdrawal(60);
+// adder.displayBalance();
+
+adder.makeDeposit(700).makeWithdrawal(20).makeWithdrawal(40).makeWithdrawal(60).displayBalance();
+
 
 percy.transferAmount(adder, 30);
 
