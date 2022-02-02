da8a49387cbf1f57906d1c9d404d8e12272ae2d5
Suhrob Hasanov <suhrobhasanov1@gmail.com>
2022-02-02 12:09:48 -0800

    Users with Bank Accounts

--- User.js
+++ User.js
@@ -1,66 +1,3 @@
-class User{
-
-    constructor(userName, email){
-        this.name = userName
-        this.email = email
-        this.accountBalance = 0 
-    }
-    makeDeposit(amount){
-        this.accountBalance += amount;
-        return this;
-    }
-    makeWithdrawal(amount){
-        this.accountBalance -= amount;
-        return this;
-    }
-    displayBalance(){
-        console.log(this.name, this.accountBalance);
-        
-    }
-    transferAmount(otherUser, amount){
-        this.accountBalance -= amount;
-        otherUser.accountBalance +=amount;
-        console.log(amount, "has been transfered to", otherUser.name);
-    }
-}
-
-const percy = new User("Percy", "lordPercyi@gmail.com");
-const baldrick = new User("Baldrick", "turnip@yahoo.com");
-const adder = new User("Adder", "blackAdder@lordship.uk");
-
-percy.name = "Lord Percy";
-
-// percy.makeDeposit(200);
-// percy.makeDeposit(300);
-// percy.makeDeposit(500);
-// percy.makeWithdrawal(50);
-// percy.displayBalance();
-
-percy.makeDeposit(200).makeDeposit(300).makeDeposit(500).makeWithdrawal(50).displayBalance();
-
-// baldrick.makeDeposit(100);
-// baldrick.makeDeposit(150);
-// baldrick.makeWithdrawal(20);
-// baldrick.makeWithdrawal(30);
-// baldrick.displayBalance();
-
-baldrick.makeDeposit(100).makeDeposit(150).makeWithdrawal(20).makeWithdrawal(30).displayBalance();
-
-// adder.makeDeposit(700);
-// adder.makeWithdrawal(20);
-// adder.makeWithdrawal(40);
-// adder.makeWithdrawal(60);
-// adder.displayBalance();
-
-adder.makeDeposit(700).makeWithdrawal(20).makeWithdrawal(40).makeWithdrawal(60).displayBalance();
-
-
-percy.transferAmount(adder, 30);
-
-percy.displayBalance();
-adder.displayBalance();
-
-
 class BankAccount {
     constructor(intRate = 0, balance = 0, accountName){
         this.accountName = accountName;
@@ -75,8 +12,8 @@ class BankAccount {
         this.balance -=amount;
         return this;
         }
-    displayAccountInfo(accountName) {
-        console.log(`Account ${accountName} balance: $${this.balance}`);
+    displayAccountInfo() {
+        console.log(`Account ${this.accountName} balance: $${this.balance}. Interest rate: ${this.intRate}`);
         }
     yieldInterest(rate) {
         this.balance += this.balance * rate;
@@ -85,8 +22,35 @@ class BankAccount {
     }
     
 
-const account1 = new BankAccount();
-const account2 = new BankAccount();
+class User{
 
-account1.deposit(10).deposit(20).deposit(30).withdraw(10).yieldInterest(0.50).displayAccountInfo("Account 1");
-account2.deposit(30).deposit(40).withdraw(10).withdraw(3).withdraw(7).withdraw(6).withdraw(4).yieldInterest(0.50).displayAccountInfo("Account 2");
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+        this.account = new BankAccount();
+    }
+    depositMethod(amount, accountName) {
+        this.account.deposit(amount);
+        console.log(`${this.name}'s account ${accountName} balance is ${this.account.balance}`);
+        return this;
+    }
+    withdrawMethod(amount) {
+        this.account.withdraw(amount);
+        return this;
+    }
+    displayUserInfo(){
+        this.displayAccountInfo(this.accountName);
+        console.log(`Name: ${this.name}. Account Balance: ${this.account.balance}`);
+        return this;
+    }
+
+}
+
+const markAccount1 = new User("Mark", "mark@bank.com");
+const markAccount2 = new User("Mark", "mark@bank.com");
+const sam = new User("Samantha", 200);
+
+
+markAccount1.depositMethod(200, "College Fund");
+markAccount2.depositMethod(300, "Rent");
+sam.depositMethod(300, "Groceries");
