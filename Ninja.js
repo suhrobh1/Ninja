7ec00e7104272639ae3c8fb3fffa7811b071c0a2
Suhrob Hasanov <suhrobhasanov1@gmail.com>
2022-02-02 19:04:14 -0800

    Super Ninja

--- Ninja.js
+++ Ninja.js
@@ -1,17 +1,17 @@
 class Ninja{
-    constructor(name, health){
+    constructor(name, health, speed = 3, strength = 3){
         this.name = name;
         this.health = health;
-        this.speed = 3;
-        this.strength = 3;
+        this.speed = speed;
+        this.strength = strength;
     }
     sayName(){
         console.log(this.name);
     }
 
-    showstats(){
+    showStats(){
         console.log(
-            "Name:", this.name,
+            "Name:", this.name, 
             "Health:", this.health,
             "Speed:", this.speed,
             "Strength:", this.strength
@@ -24,8 +24,33 @@ class Ninja{
 
 }
 
-const ninjaA = new Ninja("Kazama", 100);
 
-ninjaA.showstats();
-ninjaA.drinkSake();
-ninjaA.showstats();
+class Sensei extends Ninja{
+    constructor(name, health=200, speed = 10, strength = 10){
+        super(name, health, speed, strength);
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        super.drinkSake();
+        console.log("Drink, but keep DRY!")
+    }
+}
+
+
+console.log("---Kazama---");
+const ninjaA = new Ninja("Kazama", 100, 5, 5);
+ninjaA.showStats();
+ninjaA.drinkSake(); 
+
+console.log("---Kenji---");
+const ninjaB = new Ninja("Kenji", 100, 7, 7);
+ninjaB.showStats();
+ninjaB.drinkSake();
+ 
+console.log("---Sensei Masatoshi---");
+const senseiA = new Sensei("Masatoshi");
+senseiA.showStats();
+senseiA.speakWisdom();
+
+
+
